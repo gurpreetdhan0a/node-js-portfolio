4288f07f918319658c3c34673c791b489834a5c0
Gurpreet Singh <gurpreet.dhanoa@hotmail.com>
2020-11-08 13:26:06 +1100

    node-js-portfolio

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const data = require('./data.json')
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req,res) =>
+{
+    res.locals.projects = data.projects;
+    res.render('index');
+});
+
+app.get('/about', (req,res) =>
+{
+    res.render('about');
+});
+
+app.get('/project/:id', (req,res) =>
+{
+    const {id} = req.params;
+    res.locals.id = id;
+    res.locals.projects = data.projects;
+    res.render('project');
+});
+
+app.get('/project', (req,res) =>
+{
+    res.render('project');
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+  
+  app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.send(`<h1>${err.status}</h1>
+    <span>Error! Page not found, here is the stack trace: </span>
+    <p>${err.stack}</p>`);
+  });
+
+app.listen(3000, () =>
+{
+    console.log("Port is running at 3000");
+})

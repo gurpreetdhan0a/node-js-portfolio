f2873c5b644e6dc2462f985d35c27bcd4fe5493b
Gurpreet Singh <gurpreet.dhanoa@hotmail.com>
2020-11-13 16:50:05 +1100

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,69 @@
 const express = require('express');
-const path = require('path');
-const data = require('./data.json')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 const app = express();
+const { data } = require('./data/flashcardData.json');
+const {cards} = data;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
 
-app.get('/', (req,res) =>
+app.get('/', (req, res) =>
 {
-    res.locals.projects = data.projects;
     res.render('index');
 });
 
-app.get('/about', (req,res) =>
+app.get('/hello', (req, res) =>
 {
-    res.render('about');
+    const name = req.cookies.username;
+    if (name) return res.render('hello', {name});
+    return res.redirect('/');
+    
+})
+
+app.post('/', (req, res)=>
+{
+    res.cookie('username', req.body.username);
+    res.redirect('/hello')
 });
 
-app.get('/project/:id', (req,res) =>
+app.post('/goodbye', (req, res) =>
 {
+    res.clearCookie('username');
+    res.redirect('/');
+});
+
+app.get('/card', (req, res)=>
+{
+    const numberOfCards = cards.length;
+    const randomCard = Math.floor( Math.random() *numberOfCards);
+    res.redirect(`/card/${randomCard}?side=question`);
+});
+
+app.get('/card/:id',(req, res) =>
+{
+    const {side} = req.query;
     const {id} = req.params;
-    res.locals.id = id;
-    res.locals.projects = data.projects;
-    res.render('project');
-});
 
-app.get('/project', (req,res) =>
-{
-    res.render('project');
+    const name = req.cookies.username;
+    const text = cards[id][side];
+    const {hint} = cards[id];
+
+    if ( !side ) return res.redirect(`/card/${id}?side=question`);
+    let cardFlip = ''
+    if (side === 'question' || side === 'answer')
+    {
+    if (side === 'question') {
+        cardFlip = `${id}?side=answer`
+    } else {
+        cardFlip = `${id}?side=question`
+    }}else{
+        return res.redirect(`/card/${id}?side=question`);
+    }
+    res.render('card', {text, name, side, id, cardFlip, hint});
 });
 
 app.use((req, res, next) => {
@@ -41,14 +75,12 @@ app.use((req, res, next) => {
   app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
-    res.send(`<h1>${err.status}</h1>
-    <span>Error! Page not found, here is the stack trace: </span>
-    <p>${err.stack}</p>`);
+    res.render('error');
   });
-
+  
 const PORT = process.env.PORT || 80;
 
-app.listen(PORT, () =>
+app.listen(PORT, ()=>
 {
-    console.log(`Server is running at ${PORT}`);
-});
+    console.log(`The server is running at ${PORT}`);
+})
